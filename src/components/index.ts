4b3832000904d2ddb8849d646a3f53c42b8e1b17
Gage Tylee <gagetylee@gmail.com>
2022-07-12 16:50:19 -0700

    Add auth component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { AuthRoutes } from './auth/auth.routes';
 import { UserRoutes } from './user/user.routes';
 
 export function registerApiRoutes(router: Router, prefix: string) {
+  router.use(`${prefix}/auth`, new AuthRoutes().router);
   router.use(`${prefix}/user`, new UserRoutes().router);
 }
